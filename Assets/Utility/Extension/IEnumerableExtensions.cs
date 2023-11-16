c190291a8f9279a8c1463f96d7f9540e76fb0d2f
ZYLin <jor13579@gmail.com>
2023-11-16 16:40:27 +0800

    v1.11.16.23
    新增IEnumerableExtensions

---
+++ Assets/Utility/Extension/IEnumerableExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Loyufei
+{
+    public static class IEnumerableExtensions
+    {
+        public static void ForEach<T>(this IEnumerable<T> self, Action<T> action) 
+        {
+            foreach (var item in self) { action?.Invoke(item); }
+        }
+    }
+}
